7ede50a4722ac7bbe76c5bb2b49e9d33ca227229
steve Linz <steve.linz@gmail.com>
2021-01-27 18:59:11 -0600

    Re-did the try catch

--- Program.cs
+++ Program.cs
@@ -14,12 +14,24 @@ namespace A1_Ticketing_System_SLinz
         {
             string file = "Tickets.csv";
             StreamReader sr = new StreamReader(file);
+            StreamWriter sw = new StreamWriter(file, true);
+            // array for watching names
+            string[] watchNames = new string[9];
+            int i = 0, ticketID = 0, watchCount = 0;
+            TicketInput ticketInput = new TicketInput();
+            string current = "", watchString = "", watching = "", summary = "", inputReturn = "";
 
             try
             {
                 using (StreamReader srTest = File.OpenText(file))
                 {
-                    Console.WriteLine($"The first line of this file is {sr.ReadLine()}");
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        System.Console.WriteLine(line);
+                        current = line.Substring(0, 1);
+                    }
+                    ticketID = Int32.Parse(current); //get the current ticketID
                 }
             }
             catch (FileNotFoundException e)
@@ -35,21 +47,6 @@ namespace A1_Ticketing_System_SLinz
                 Console.WriteLine($"The file could not be opened: '{e}'");
             }
 
-            StreamWriter sw = new StreamWriter(file, true);
-            // array for watching names
-            string[] watchNames = new string[9];
-            int i = 0, ticketID = 0, watchCount = 0;
-            TicketInput ticketInput = new TicketInput();
-            string current = "", watchString = "", watching = "", summary = "", inputReturn = "";
-
-            while (!sr.EndOfStream)
-            {
-                string line = sr.ReadLine();
-                System.Console.WriteLine(line);
-                current = line.Substring(0, 1);
-            }
-            ticketID = Int32.Parse(current); //get the current ticketID
-                                    
 
             while (true)
             {
@@ -95,9 +92,7 @@ namespace A1_Ticketing_System_SLinz
                 watchString = "";
                 Array.Clear(watchNames, 0, i);
             }
-
             sw.Close();
-            
         }
 
     }
