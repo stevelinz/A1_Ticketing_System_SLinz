f79aed2775bca0aace12a55199224a01dff33d32
steve Linz <steve.linz@gmail.com>
2021-01-29 14:51:59 -0600

    fixed ticket id to hold up to 99999, and add more records

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text.RegularExpressions;
 namespace A1_Ticketing_System_SLinz
 {
     /*
@@ -16,9 +17,10 @@ namespace A1_Ticketing_System_SLinz
             StreamReader sr = new StreamReader(file);
             // array for watching names
             string[] watchNames = new string[9];
+            int  ticketLife = 4; // system will never hold more than 99,999 records.
             int i = 0, ticketID = 0, watchCount = 0;
             TicketInput ticketInput = new TicketInput();
-            string current = "", watchString = "", watching = "", 
+            string str2 = "", watchString = "", watching = "",
             summary = "", inputReturn = "", start = "";
 
             try
@@ -29,9 +31,15 @@ namespace A1_Ticketing_System_SLinz
                     {
                         string line = sr.ReadLine();
                         System.Console.WriteLine(line);
-                        current = line.Substring(0, 1);
+                        string str1 = line;
+                        str2 = string.Empty;
+                        for (int x = 0; x < ticketLife; x++)
+                        {
+                            if (Char.IsDigit(str1[x]))
+                                str2 += str1[x];
+                        }
                     }
-                    ticketID = Int32.Parse(current); //get the current ticketID
+                    ticketID = int.Parse(str2); //get the current ticketID
                 }
             }
             catch (FileNotFoundException e)
@@ -50,13 +58,22 @@ namespace A1_Ticketing_System_SLinz
             StreamWriter sw = new StreamWriter(file, true);
             Info info = new Info();
             info.requirements();
-            System.Console.WriteLine("Press any key to enter the A1-Ticket process");
+            System.Console.WriteLine("Press Enter key to enter the A1-Ticket process");
             start = Console.ReadLine();
-            
+
             while (true)
             {
                 Console.Write("Summary of this ticket, or type D if done: ");
                 summary = Console.ReadLine();
+
+                while (digitsFound(summary)) 
+                {
+                    Console.Write("Summary of this ticket, numbers NOT allowed. type D if done: ");
+                    summary = Console.ReadLine();
+                    if (digitsFound(summary)) break;
+                }
+
+
                 if (summary == "D" || summary == "d")
                 {
                     sw.Close();
@@ -70,6 +87,7 @@ namespace A1_Ticketing_System_SLinz
 
                 Console.Write("Watching this ticket: You add can up to 8 people, type D if done: ");
                 watching = Console.ReadLine();
+
                 for (i = 0; i < 9; i++)
                 {
                     if (watching == "d" || watching == "D" || watchCount > 8)
@@ -91,6 +109,19 @@ namespace A1_Ticketing_System_SLinz
                         Console.Write("Watching this ticket: You add can up to 8 people, type D if done: ");
                         watching = Console.ReadLine();
                     }
+
+                }
+
+                bool digitsFound(string summary)  // to make sure the ticketID remains correct
+                {             
+                    foreach (char c in summary)
+                    {    
+                            if (c >= '0' && c <= '9')
+                            {
+                              return true;
+                            }
+                    }
+                    return false;
                 }
 
                 sw.WriteLine("{0},{1},{2},{3}", ticketID, summary, inputReturn, watchString);
